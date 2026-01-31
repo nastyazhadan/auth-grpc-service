280389a2ff331ce3c3bed7c457fe6152e13123d3
Anastasia Zhadan <nastya.zhadan2017@yandex.ru>
2026-02-01 00:42:21 +0300

    add config and logger

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,11 +1,46 @@
 package main
 
-func main() {
-	// TODO: инициализировать объекта конфига
+import (
+	"authGRPCService/internal/config"
+	"log/slog"
+	"os"
+)
 
-	// TODO: инициализировать логгер
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application", slog.Any("cfg", cfg))
 
 	// TODO: инициализировать приложение (app)
 
 	// TODO: запустить grpc-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
